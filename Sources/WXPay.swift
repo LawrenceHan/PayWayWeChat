9679b0d3b9bc427806a99269d21d9e4416931fc1
LawrenceHan <yangfei6565@163.com>
2019-11-13 18:58:36 +0800

    added comments

--- Sources/WXPay.swift
+++ Sources/WXPay.swift
@@ -10,10 +10,6 @@
 import Foundation
 import WeChatSDK
 
-let _appId = "wxb4ba3c02aa476ea1"
-let _universalLink = "https://help.wechat.com/sdksample/"
-let _url = "https://wxpay.wxutil.com/pub_v2/app/app_pay.php?plat=ios"
-
 public final class WXPay: NSObject {
     
     // MARK: - Public
@@ -24,7 +20,7 @@ public final class WXPay: NSObject {
     
     public func registerApp(appId: String, universalLink: String) {
         if isDebug {
-            WXApi.registerApp(_appId, universalLink: _universalLink)
+            WXApi.registerApp(debug_appId, universalLink: debug_universalLink)
         } else {
             WXApi.registerApp(appId, universalLink: universalLink)
         }
@@ -35,18 +31,22 @@ public final class WXPay: NSObject {
     }
     
     public func prePayRequest(with request: URLRequest, completion: @escaping (Result<WXPayResponse, WXPayError>) -> Void) -> URLSessionTask? {
+        
+        // clean _completion
         if _completion != nil {
             _completion = nil
             print("[PayWayWeChat] only support one wxpay request at a time, cancelled last one.")
         }
         
+        // debug setup
         var request = request
         if isDebug {
-            request = URLRequest(url: URL(string: _url)!)
+            request = URLRequest(url: URL(string: debug_url)!)
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         }
         
-        let task = URLSession.shared.dataTask(with: request) { (data, urlResponse, error) in
+        // create data task
+        let task = URLSession.shared.dataTask(with: request) { data, urlResponse, error in
             let result: Result<PayReqContainer, WXPayError>
             
             switch (data, urlResponse, error) {
@@ -69,12 +69,16 @@ public final class WXPay: NSObject {
                 result = .failure(.responseError(ResponseError.nonHTTPURLResponse(urlResponse)))
             }
             
+            // Got result from backend, open WeChat app if success
             switch result {
             case .success(let payReq):
                 DispatchQueue.main.async { [unowned self] in
                     WXApi.send(payReq.payRequest) { result in
                         if result {
+                            // save the completion for later WeChat callback
                             self._completion = completion
+                            
+                            // Setup WeChat callback timeout function
                             DispatchQueue.main.asyncAfter(deadline: .now() + self.callbackTimeout) {
                                 #if DEBUG
                                 print("[PayWayWeChat] timeout callback")
@@ -103,6 +107,9 @@ public final class WXPay: NSObject {
     // MARK: - Private
     
     private var _completion: ((Result<WXPayResponse, WXPayError>) -> Void)?
+    private let debug_appId = "wxb4ba3c02aa476ea1"
+    private let debug_universalLink = "https://help.wechat.com/sdksample/"
+    private let debug_url = "https://wxpay.wxutil.com/pub_v2/app/app_pay.php?plat=ios"
 }
 
 // MARK: - WXApi delegate
@@ -111,8 +118,11 @@ extension WXPay: WXApiDelegate {
     public func onReq(_ req: BaseReq) {
     }
     
+    // received callback from WeChat app
     public func onResp(_ resp: BaseResp) {
         let payload = WXPayResponse(code: Int(resp.errCode), message: resp.errStr, type: Int(resp.type))
+        
+        // send notification out
         NotificationCenter.default.post(name: .newWXPayResp, object: payload)
         
         let result: Result<WXPayResponse, WXPayError>
